05e4f18b52fb1deda4f9c028422d0bde808009d0
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2024-01-10 20:31:08 +0400

    update

--- script.js
+++ script.js
@@ -1,132 +1,134 @@
-const movieContainer = document.querySelector("#movieContainer");
-const mySearchBtn = document.querySelector("#searchBtn");
+const movieContainer = document.querySelector("#movieContainer")
+const searchBtn = document.querySelector("#searchBtn")
 const myInputArea = document.querySelector("#myInputArea")
-const container = document.querySelector("#container")
 
-mySearchBtn.addEventListener("click", searchFilm)
-myInputArea.addEventListener("keyup", enterSearch)
-showFilm()
+window.addEventListener('DOMContentLoaded', showMoviesUI)
+searchBtn.addEventListener('click', searchFilm)
+myInputArea.addEventListener('keyup', searchFilmWithEnter)
 
-function enterSearch(e){
-  if(e.keyCode === 13){
-    searchFilm()
-  }
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjE5ODQ5NjMxYTRlMWZiYzVlMDIzYWUwMDc4ZDgzOSIsInN1YiI6IjY0NjdmNmIwMmJjZjY3MDEzODk0MzM2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dyQbf0TAGw-xO-uOrPoWL_okGTOkcJlklzGraLB8-d8'
+    }
+};
+
+async function getMoviesData() {
+
+    try {
+        const moviesData = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+        return await moviesData.json()
+    } catch (err) {
+        console.log(err);
+    }
+
+
 }
 
-function showFilm() {
-  let newJson = fetch("./myJson.json")
+async function showMoviesUI() {
+    const movies = await getMoviesData()
 
-  newJson
-    .then(response => {
-      return response.json()
-    }).then(response => {
-      let { results } = response
-      results.forEach((data) => {
+    movieContainer.innerHTML += movies.results.map((movie) => (
+        `<div class="card mx-5 my-5" style="width: 18rem;">
+        <img src="https://image.tmdb.org/t/p/w1280${movie.poster_path}" class="card-img-top" style="height:300px">
+        <div class="card-body">
+          <h5 class="card-title">${movie.original_title}</h5>
+          <p class="card-text">${movie.overview.slice(0, 100) + '...'}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Dil: ${movie.original_language}</li>
+          <li class="list-group-item">IMDB: ${movie.vote_average}</li>
+          <li class="list-group-item">Yayınlanma Tarixi: ${movie.release_date}</li>
+        </ul>
+        <div class="card-body">
+          <button class="btn btn-danger" onclick="showFilmDetail(${movie.id})">Watch the Film</button>
+        </div>
+      </div>`
+    ))
 
-        let newMovie = `<div class="card mx-5 my-5" style="width: 18rem;">
-      <img src="${data["backdrop_path"]}" class="card-img-top" style="height:300px">
-      <div class="card-body">
-        <h5 class="card-title">${data["original_title"]}</h5>
-        <p class="card-text">${data["overview"].substring(0,200)}</p>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Dil: ${data['original_language']}</li>
-        <li class="list-group-item">IMDB: ${data['vote_average']}</li>
-        <li class="list-group-item">Yayınlanma Tarixi: ${data['release_date']}</li>
-      </ul>
-      <div class="card-body">
-        <a href="#" class="btn btn-danger" onclick="showNewFilm('${data['film_link']}')">Watch the Film</a>
-      </div>
-    </div>`
-        movieContainer.innerHTML += newMovie
-      })
-
-    })
 }
 
+async function getFilmWithID(movieID) {
+    try {
+        const moviesData = await fetch(`https://api.themoviedb.org/3/movie/${movieID}/watch/providers`, options)
+        const movieDetail = await moviesData.json()
+        return movieDetail
+    } catch (err) {
+        console.log(err);
+    }
+}
 
-function showNewFilm(film) {
+async function showFilmDetail(movieId) {
+    const movieDetail = await getFilmWithID(movieId)
+    console.log(movieDetail);
+    let { results } = movieDetail
 
-  window.open(film, '_blank')
+    window.open(results.US.link, '_blank')
+}
+
+async function getSearchFilm(searchText) {
+    try {
+        const moviesData = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchText}&include_adult=false&language=en-US&page=1`, options)
+        const searchMovie = await moviesData.json()
+        return searchMovie
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 async function searchFilm() {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjE5ODQ5NjMxYTRlMWZiYzVlMDIzYWUwMDc4ZDgzOSIsInN1YiI6IjY0NjdmNmIwMmJjZjY3MDEzODk0MzM2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dyQbf0TAGw-xO-uOrPoWL_okGTOkcJlklzGraLB8-d8'
-    }
-  };
+    const searchText = getSearchInputValue()
+    const searchMovie = await getSearchFilm(searchText)
+    let { results } = searchMovie
 
-  let myInput = getInputArea()
-
-  let newPromise = await fetch(`https://api.themoviedb.org/3/search/movie?query=${myInput}&include_adult=false&language=en-US&page=1`, options)
-  let result = await newPromise.json()
-  console.log(result);
-  let { results } = result
-  console.log(results);
-
-  try {
-    console.log(results.length);
-    if (results.length) {
-      movieContainer.innerHTML = "";
-      setFilm(results)
+    if (results.length > 0) {
+        movieContainer.innerHTML = ''
+        myInputArea.value = ''
+        showSearchMovieUI(results)
     } else {
-      showErr()
+        movieContainer.innerHTML = ''
+        myInputArea.value = ''
+        showErr()
     }
-  } catch (err) {
-  }
-}
-
-function setFilm(results) {
-
-
-  results.forEach((data) => {
-    let imgUrl = "https://www.themoviedb.org/t/p/w1280"
-    if(data["poster_path"] === null){
-     data["poster_path"] = ""
-     imgUrl = "https://shorturl.at/imnY6"
-    }
-    let newMovie = `<div class="card mx-5 my-5" style="width: 18rem;">
-<img src="${imgUrl}${data["poster_path"]}" class="card-img-top" style="height:300px">
-<div class="card-body">
-  <h5 class="card-title">${data["original_title"]}</h5>
-  <p class="card-text">${data["overview"].substring(0,200)}</p>
-</div>
-<ul class="list-group list-group-flush">
-  <li class="list-group-item">Dil: ${data['original_language']}</li>
-  <li class="list-group-item">IMDB: ${data['vote_average']}</li>
-  <li class="list-group-item">Yayınlanma Tarixi: ${data['release_date']}</li>
-</ul>
-<div class="card-body">
-  <a href="#" class="btn btn-danger" onclick="showNewFilm('${data['film_link']}')">Watch the Film</a>
-</div>
-</div>`
-    movieContainer.innerHTML += newMovie
-  })
 }
 
 function showErr() {
-  movieContainer.innerHTML = "";
-  let errMessage = document.createElement("div");
-  errMessage.classList = "text-bg-danger fs-3 mt-5 fw-bold"
-  errMessage.innerHTML = "Xəta! Axtardığınız film yoxdur...";
-
-  container.appendChild(errMessage)
-  console.log(container);
-  setTimeout(() => {
-    container.removeChild(container.lastElementChild)
-    showFilm()
-  }, 2000)
+    movieContainer.innerHTML = `<div class="text-bg-danger fs-3 mt-5 fw-bold">
+      Xəta! Axtardığınız film yoxdur...
+    </div>`
+    setTimeout(() => {
+        movieContainer.innerHTML = ''
+        showMoviesUI()
+    },2000)
 }
 
-
-function getInputArea() {
-  let myInput = myInputArea.value.trim();
-  if (myInput.length > 3) {
-    myInputArea.value = ""
-    return myInput
-  }
+function showSearchMovieUI(results) {
+    movieContainer.innerHTML += results.map((movie) => (
+        `<div class="card mx-5 my-5" style="width: 18rem;">
+        <img src="https://image.tmdb.org/t/p/w1280${movie.poster_path}" class="card-img-top" style="height:300px">
+        <div class="card-body">
+          <h5 class="card-title">${movie.original_title}</h5>
+          <p class="card-text">${movie.overview.slice(0, 100) + '...'}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Dil: ${movie.original_language}</li>
+          <li class="list-group-item">IMDB: ${movie.vote_average}</li>
+          <li class="list-group-item">Yayınlanma Tarixi: ${movie.release_date}</li>
+        </ul>
+        <div class="card-body">
+          <button class="btn btn-danger" onclick="showFilmDetail(${movie.id})">Watch the Film</button>
+        </div>
+      </div>`
+    ))
 }
 
+function getSearchInputValue() {
+    return myInputArea.value.trim().toLowerCase()
+}
+
+function searchFilmWithEnter(e) {
+    if(e.keyCode === 13) {
+        searchFilm()
+    }
+}
